7b03cb5f7fe094cd24a8c04577246c6c14154b57
wassii-khan <wassiikhan933@gmail.com>
2025-05-04 00:45:24 +0500

    feat: Add axios configuration for API client

---
+++ client/src/config.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export const apiClient = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  timeout: 10000,
+});
+
+// authentication flag
+export const isAccountCreated = false;
