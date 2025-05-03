9bce9b786934a6903eabf8c98f9c0085c2ad41ce
wassii-khan <wassiikhan933@gmail.com>
2025-05-03 16:30:25 +0500

    feat: Implement CreateAccount and chat room management functions

---
+++ server/controllers/chat/chat-controller.js
@@ -0,0 +1,8 @@
+// create chat rooms
+export const CreateRoom = (req, res) => {
+  res.status(200).send("Hello, world");
+};
+// get chat rooms
+export const GetRooms = (req, res) => {
+  res.status(200).send("Hello, world");
+};
