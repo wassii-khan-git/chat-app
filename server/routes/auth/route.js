dc8d46d1f0d4e99993e5231507af1953fae08962
wassii-khan <wassiikhan933@gmail.com>
2025-05-04 00:50:36 +0500

    feat: Add token generation and verification utilities

--- server/routes/auth/route.js
+++ server/routes/auth/route.js
@@ -1,9 +1,14 @@
 import express from "express";
 import { CreateAccount } from "../../controllers/auth/auth-controller.js";
+import { VerifyUserToken } from "../../middlewares/auth/auth.middleware.js";
 
 const router = express.Router();
 
 // create user account
 router.post("/create-account", CreateAccount);
+// create all users
+router.get("/all-users", VerifyUserToken, (req, res) => {
+  res.status(200).json({ message: "everything is fine" });
+});
 
 export default router;
---
+++ server/utils/helper.js
@@ -0,0 +1,12 @@
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../config/config.js";
+
+// generate token
+export const GenerateToken = async (id) => {
+  return jwt.sign({ userId: id }, JWT_SECRET, { expiresIn: "1w" });
+};
+
+// verify token
+export const VerifyToken = async (token) => {
+  return jwt.verify(token, JWT_SECRET);
+};
