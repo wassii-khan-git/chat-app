da7c81e78aa0812b2e20abcb2a6b6b66e6b99785
wassii-khan <wassiikhan933@gmail.com>
2025-05-04 00:49:50 +0500

    feat: Add JWT_SECRET export for JWT authentication

--- server/config/config.js
+++ server/config/config.js
@@ -6,3 +6,6 @@ export const DATABASE_URL = process.env.DATABASE_URL;
 
 // PORT NUMBER
 export const PORT_NUMBER = process.env.PORT_NUMBER || 5000;
+
+// JWT SECRET
+export const JWT_SECRET = process.env.JWT_SECRET;
