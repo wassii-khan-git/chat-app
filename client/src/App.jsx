3d520f79540a5fd71af65e68feb709233586b683
wassii-khan <wassiikhan933@gmail.com>
2025-05-04 00:44:43 +0500

    feat: Integrate QueryClientProvider for React Query support in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,12 @@
 import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/home";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
+  // Create a QueryClient instance
+  const queryClient = new QueryClient();
+
   // Router setup
   const router = createBrowserRouter([
     {
@@ -10,8 +14,15 @@ function App() {
       element: <Home />,
     },
   ]);
-  // RouterProvider setup
-  return <RouterProvider router={router} />;
+
+  // RouterProvider setup with QueryClientProvider
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router}>
+        {/* Your app components go here */}
+      </RouterProvider>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
