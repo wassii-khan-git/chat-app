d031458cacd91b079b80f10fea272249145fc8bf
wassii-khan <wassiikhan933@gmail.com>
2025-05-03 16:30:19 +0500

    feat: Add configuration file to load environment variables for database URL and port number

---
+++ server/config/config.js
@@ -0,0 +1,8 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+// Database URL
+export const DATABASE_URL = process.env.DATABASE_URL;
+
+// PORT NUMBER
+export const PORT_NUMBER = process.env.PORT_NUMBER || 5000;
