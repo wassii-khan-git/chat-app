cde76aafe3358b383a3543b3fceb6add284f7cc1
wassii-khan <wassiikhan933@gmail.com>
2025-05-04 15:48:29 +0500

    feat: Implement LoginApi for user authentication

--- client/src/services/auth/auth.services.js
+++ client/src/services/auth/auth.services.js
@@ -10,3 +10,14 @@ export const CreateAccountApi = async (form) => {
     throw error.response ? error.response.data : error;
   }
 };
+
+// create account
+export const LoginApi = async (form) => {
+  try {
+    const response = await apiClient.post("/auth/login", form);
+    return response.data;
+  } catch (error) {
+    // You can throw the error to be handled by the caller
+    throw error.response ? error.response.data : error;
+  }
+};
