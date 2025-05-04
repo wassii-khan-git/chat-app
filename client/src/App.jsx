e4d5debb47d9bffc6522d76b479dc06b89b5a1a0
wassii-khan <wassiikhan933@gmail.com>
2025-05-04 20:52:11 +0500

    feat: Simplify routing by removing RouterProvider and integrating CustomRoute

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,43 +1,14 @@
 import "./App.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./pages/home";
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import Login from "./components/login";
-import ChatLayout from "./components/layouts/chatLayout";
+import CustomRoute from "./routes";
 
 function App() {
-  // Create a QueryClient instance
   const queryClient = new QueryClient();
 
-  // Router setup
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <ChatLayout />,
-      children: [
-        {
-          index: true,
-          path: "/login",
-          element: <Login />,
-        },
-        {
-          path: "/signup",
-          element: <CreateAccount />,
-        },
-        {
-          path: "/dashboard",
-          element: <Home />,
-        },
-      ],
-    },
-  ]);
-
-  // RouterProvider setup with QueryClientProvider
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router}>
-        {/* Your app components go here */}
-      </RouterProvider>
+      <CustomRoute />
     </QueryClientProvider>
   );
 }
