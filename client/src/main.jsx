78960762e1e9bf44316348ad2e2c1b76e3618868
wassii-khan <wassiikhan933@gmail.com>
2025-05-04 20:52:17 +0500

    feat: Wrap App component with BrowserRouter for routing support

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./hooks/provider";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <AuthProvider>
-    <StrictMode>
-      <App />
-    </StrictMode>
+    <BrowserRouter>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </BrowserRouter>
   </AuthProvider>
 );
