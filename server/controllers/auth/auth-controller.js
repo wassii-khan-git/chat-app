0ffb519dd843b517fdca2d1236ddf006e55f2eec
wassii-khan <wassiikhan933@gmail.com>
2025-05-04 12:49:51 +0500

    feat: Enhance account creation with password validation and duplicate checks

--- server/controllers/auth/auth-controller.js
+++ server/controllers/auth/auth-controller.js
@@ -6,7 +6,52 @@ export const CreateAccount = async (req, res) => {
   try {
     console.log("i am body", req.body);
     // get the values
-    const { username, email } = req.body;
+    const { username, email, password } = req.body;
+    // username and email is empty
+    if (!username || !email || !password) {
+      return res.status(409).json({
+        success: false,
+        message: "Username or email or password is required",
+      });
+    }
+
+    // Check for the duplicates
+    const isUserExists = await UserModel.findOne({
+      email: email,
+      username: username,
+    });
+
+    // if the user already exists
+    if (isUserExists) {
+      return res.status(409).json({
+        success: false,
+        message: "Username or email already exists",
+      });
+    }
+
+    // store it in the db
+    const newUser = await UserModel.create({
+      username,
+      email,
+      password,
+    });
+
+    // return the response
+    return res.status(201).json({
+      success: true,
+      message: "Account created successfully",
+      user: newUser,
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+// Login Account
+export const Login = async (req, res) => {
+  try {
+    console.log("i am body", req.body);
+    // get the values
+    const { email, password } = req.body;
     // username and email is empty
     if (!username || !email) {
       return res.status(409).json({
@@ -46,7 +91,6 @@ export const CreateAccount = async (req, res) => {
       success: true,
       message: "Account created successfully",
       user: newUser,
-      token,
     });
   } catch (error) {
     res.status(500).json({ success: false, message: error.message });
--- server/models/user-model.js
+++ server/models/user-model.js
@@ -17,6 +17,11 @@ const UserSchema = new mongoose.Schema(
         message: "Invalid email format",
       },
     },
+    password: {
+      type: String,
+      required: true,
+      unique: true,
+    },
     token: {
       type: String,
       unique: true,
