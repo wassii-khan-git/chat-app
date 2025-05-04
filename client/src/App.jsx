4ab6e0b55e446232be07b01e9108939f540e1b3d
wassii-khan <wassiikhan933@gmail.com>
2025-05-04 15:48:00 +0500

    feat: Update routing structure to include ChatLayout and nested routes for login, signup, and dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/home";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Login from "./components/login";
+import ChatLayout from "./components/layouts/chatLayout";
 
 function App() {
   // Create a QueryClient instance
@@ -11,7 +13,22 @@ function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Home />,
+      element: <ChatLayout />,
+      children: [
+        {
+          index: true,
+          path: "/login",
+          element: <Login />,
+        },
+        {
+          path: "/signup",
+          element: <CreateAccount />,
+        },
+        {
+          path: "/dashboard",
+          element: <Home />,
+        },
+      ],
     },
   ]);
 
