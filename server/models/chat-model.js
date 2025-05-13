2a51cfc1edcaa39fc7dfe34a876c09a2ecd89ab0
wassii-khan <wassiikhan933@gmail.com>
2025-05-13 22:05:39 +0500

    feat: Define ChatSchema model for chat messages with necessary fields

--- server/models/chat-model.js
+++ server/models/chat-model.js
@@ -1,1 +1,31 @@
- 
+import mongoose, { Schema } from "mongoose";
+
+const ChatSchema = new Schema(
+  {
+    roomId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    sender: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "users",
+    },
+    receiver: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "users",
+    },
+    seen: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+export const ChatModel = mongoose.model("chats", ChatSchema);
