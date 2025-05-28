1dcba6a3be7ca4d0a53027a53d7f65097beb6d1f
wassii-khan <wassiikhan933@gmail.com>
2025-05-28 19:10:41 +0500

    fix: Enhance error handling for unauthorized access in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,14 +5,17 @@ import CustomRoute from "./routes";
 import { useEffect } from "react";
 import { apiClient } from "./config";
 import { useAuth } from "./hooks/index";
+import { useNavigate } from "react-router-dom";
 
 // App Component
 function App() {
   const queryClient = new QueryClient();
-  const { auth } = useAuth();
+  const { auth, logout } = useAuth();
   const token = auth?.user?.token;
+  const navigate = useNavigate();
 
   useEffect(() => {
+    // for requests
     apiClient.interceptors.request.use(
       (config) => {
         if (token) {
@@ -24,7 +27,23 @@ function App() {
         Promise.reject(error);
       }
     );
-  }, [auth, token]);
+    apiClient.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (error) => {
+        if (error.response.status === 401) {
+          // Handle unauthorized access
+          console.error("Unauthorized access - redirecting to login");
+          // You can redirect to login page or show a message
+          logout();
+          // Optionally, you can redirect to a login page
+          navigate("/login", { replace: true });
+        }
+        return Promise.reject(error);
+      }
+    );
+  }, [auth, token, logout, navigate]);
 
   return (
     <QueryClientProvider client={queryClient}>
